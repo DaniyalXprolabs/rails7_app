5d0d3d48d10531ef92eaa0d545f47e5617819c5a
DaniyalXprolabs <daniyal.malik@xprolabs.com>
2022-08-24 11:17:20 +0500

    RichText: Completed the rich text configuration for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :body, presence: true, length: { minimum: 20, maximum: 2000 }
 end
