5d1d51c6038f1b447aad5f001653e47cd53184b0
DaniyalXprolabs <daniyal.malik@xprolabs.com>
2022-08-25 14:03:11 +0500

    Comment: Added update action for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@
 
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
-  before_action :find_post, only: %i[create destroy index]
+  before_action :find_post, only: %i[create destroy index find_comment update]
+  before_action :find_comment, only: %i[destroy update]
 
   def index
     @comments = @post.comments.all
@@ -20,15 +21,28 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
+  end
+
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to post_path(@post), notice: 'Comment updated successfully'}
+      else
+        format.html { redirect_to post_path(@post), alert: 'Comment not updated successfully'}
+      end
+    end
   end
 
   private
 
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def find_comment
+    @comment = @post.comments.find(params[:id])
   end
 
   def comment_params
