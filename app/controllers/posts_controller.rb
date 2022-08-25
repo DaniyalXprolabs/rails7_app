dca8c71b6c03bff6d869dd4fadb981b0063cc58a
DaniyalXprolabs <daniyal.malik@xprolabs.com>
2022-08-25 12:21:12 +0500

    Comments: Created instance variable for comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   # GET /posts/1 or /posts/1.json
   def show
     @post.update(views: @post.views + 1)
-    @comment = @post.comments.all(created_at: :desc)
+    @comments = @post.comments.all
   end
 
   # GET /posts/new
